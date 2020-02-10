9262afcc06ed473a1aa429ec88eb653aba45ba98
serhiichernukha <sooflex77@gmai.com>
2020-02-10 14:48:34 +0200

    add backlog description

---
+++ Task 1/variables.tf
@@ -0,0 +1,32 @@
+variable "project" {
+  description = "Project unic ID"
+  default = "gcptask-256312"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-a"
+}
+
+variable "default_network" {
+  description = "Default VPC network"
+  type = string
+  default = "projects/gcptask-256312/global/networks/default"
+}
+
+variable "image" {
+  default = {
+    "us-central1" = "centos-7-v20191014"
+      }
+}
+
+variable "mtype" {
+  default = "n1-standard-1"
+}
+
+variable "servername" {
+  default = "appserver"
+}
